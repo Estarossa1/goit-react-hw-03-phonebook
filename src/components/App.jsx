6e7feeadab4ea91575fd5ff5af96b62ed6da9476
Oleksandr Zabolotnuy <118597762+Estarossa1@users.noreply.github.com>
2023-07-06 11:00:32 +0300

    end progect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,31 @@ import css from './App.module.css';
 export class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
   };
+
+  // save in Local storage
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(this.storageKey);
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem(this.storageKey, JSON.stringify(contacts));
+    }
+  }
+
+  // save in Local storage end
 
   handleFilterChange = event => {
     this.setState({ filter: event.target.value });
